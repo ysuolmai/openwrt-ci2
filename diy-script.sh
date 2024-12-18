758024aadf99c1ced316468f986c8fd319545239
ysuolmai <fan0417@gmail.com>
2024-12-19 07:01:20 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -160,29 +160,6 @@ for line in "${provided_config_lines[@]}"; do
     echo "$line" >> .config
 done
 
-PKG_PATCH="$GITHUB_WORKSPACE/wrt/package/"
-
-#预置HomeProxy数据
-if [ -d *"homeproxy"* ]; then
-	HP_RULES="surge"
-	HP_PATCH="homeproxy/root/etc/homeproxy"
-
-	chmod +x ./$HP_PATCH/scripts/*
-	rm -rf ./$HP_PATCH/resources/*
-
-	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$HP_RULES/
-	cd ./$HP_RULES/ && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
-
-	echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
-	awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
-	sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
-	mv -f ./{china_*,gfw_list}.{ver,txt} ../$HP_PATCH/resources/
-
-	cd .. && rm -rf ./$HP_RULES/
-
-	cd $PKG_PATCH && echo "homeproxy date has been updated!"
-fi
-
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
