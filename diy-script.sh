e7649015fe8e5496770eba9ccee171effc40171c
ysuolmai <fan0417@gmail.com>
2025-01-24 06:22:19 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -103,7 +103,7 @@ UPDATE_PACKAGE "luci-app-adguardhome" "https://github.com/ysuolmai/luci-app-adgu
 keywords_to_delete=(
     "xiaomi_ax3600" "xiaomi_ax9000" "xiaomi_ax1800" "glinet" "jdcloud_ax6600"
     "mr7350" "uugamebooster" "luci-app-wol" "luci-i18n-wol-zh-cn" "CONFIG_TARGET_INITRAMFS" "ddns" "LSUSB" "mihomo"
-    "argon"
+    "argon" "smartdns"
 )
 
 
