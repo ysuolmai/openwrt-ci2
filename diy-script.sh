dd099a79995992a40a5a9a375ee21315112f7553
ysuolmai <fan0417@gmail.com>
2025-04-07 06:44:13 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -162,8 +162,8 @@ if [[ $FIRMWARE_TAG == *"NOWIFI"* ]]; then
     #    -e 's/reg = <0x0 0x4b000000 0x0 0x3700000>;/reg = <0x0 0x4b000000 0x0 0x1000000>;/' {} +
     #find $DTS_PATH -type f ! -iname '*nowifi*' -exec sed -i 's/ipq\(6018\|8074\).dtsi/ipq\1-nowifi.dtsi/g' {} +
     find "$DTS_PATH" -type f ! -iname '*nowifi*' -exec sed -i \
-      -e '/#include "ipq6018.dtsi"/a #include "ipq6018-nowifi.dtsi"' {} +
-      #-e '/#include "ipq8074.dtsi"/a #include "ipq8074-nowifi.dtsi"' {} +
+      -e '/#include "ipq6018.dtsi"/a #include "ipq6018-nowifi.dtsi"'
+      -e '/#include "ipq8074.dtsi"/a #include "ipq8074-nowifi.dtsi"' {} +
     echo "qualcommax set up nowifi successfully!"
 
 else
@@ -188,11 +188,11 @@ fi
 
 
 # 只有 $FIRMWARE_TAG 不包含 'EMMC' 且包含 'WIFI-NO' 时执行删除命令
-if [[ "$FIRMWARE_TAG" != *"EMMC"* && "$FIRMWARE_TAG" == *"NOWIFI"* ]]; then
-    sed -i 's/\s*kmod-[^ ]*usb[^ ]*\s*\\\?//g' ./target/linux/qualcommax/Makefile
-    sed -i 's/\s*kmod-[^ ]*ath11k[^ ]*\s*\\\?//g' ./target/linux/qualcommax/Makefile
-    echo "已删除 Makefile 中的 USB 相关 package"
-fi
+#if [[ "$FIRMWARE_TAG" != *"EMMC"* && "$FIRMWARE_TAG" == *"NOWIFI"* ]]; then
+#    sed -i 's/\s*kmod-[^ ]*usb[^ ]*\s*\\\?//g' ./target/linux/qualcommax/Makefile
+#    sed -i 's/\s*kmod-[^ ]*ath11k[^ ]*\s*\\\?//g' ./target/linux/qualcommax/Makefile
+#    echo "已删除 Makefile 中的 USB 相关 package"
+#fi
 
 rm package/kernel/mac80211/patches/nss/ath11k/999-902-ath11k-fix-WDS-by-disabling-nwds.patch
 rm package/kernel/mac80211/patches/nss/subsys/{999-775-wifi-mac80211-Changes-for-WDS-MLD.patch,999-922-mac80211-fix-null-chanctx-warning-for-NSS-dynamic-VLAN.patch}
