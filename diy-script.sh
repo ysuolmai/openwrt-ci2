14479cb5bc86ac43fe3ba23c1be314f82f8427be
ysuolmai <fan0417@gmail.com>
2025-08-05 17:43:20 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -82,7 +82,7 @@ sed -i '/ntfs-3g-utils /d' package/luci-app-diskman/Makefile
 mkdir -p package/parted && \
 wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
 
-
+UPDATE_PACKAGE "frp" "https://github.com/ysuolmai/openwrt-frp.git" "main"
 
 keywords_to_delete=(
     "xiaomi_ax3600" "xiaomi_ax9000" "xiaomi_ax1800" "glinet" "jdcloud_ax6600"
